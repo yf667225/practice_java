eb1bbb1d2e883824aec09f63b16c240eed801a7a
yangfeng <1311423477@qq.com>
2021-08-08 21:46:07 +0800

    homework_1-20210808_yangfeng

---
+++ week_1/pratice/src/main/java/com/chris/pratice/PraticeApplication.java
@@ -0,0 +1,13 @@
+package com.chris.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework1Application.class, args);
+	}
+
+}
